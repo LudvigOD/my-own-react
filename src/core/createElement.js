793bb57d8822e7916a46df8eac21f2de5ab70d8b
LudvigOD <144817122+LudvigOD@users.noreply.github.com>
2025-05-26 23:17:11 +0200

    Update createElement.js

--- src/core/createElement.js
+++ src/core/createElement.js
@@ -1,11 +1,12 @@
 import createTextElement from "./createTextElement.js";
 
+// creates an Element (text, or other), takes properties and (if any) children
 export default function createElement(type, props, ...children) {
   return {
     type,
     props: {
       ...props,
-      children: children.map(child =>
+      children: children.map(child => // it is text or an other type of element
         typeof child === "object"
           ? child
           : createTextElement(child)
