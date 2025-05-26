f62c55739f894a2731a86fc85195e53f7eeb2288
LudvigOD <144817122+LudvigOD@users.noreply.github.com>
2025-05-27 01:28:29 +0200

    Update useState.js

--- src/hooks/useState.js
+++ src/hooks/useState.js
@@ -2,23 +2,31 @@ import * as state from "../global/state.js"
 
 export default function useState(initial) {
   const wFiber = state.getWipFiber()
+  // get oldHook from alternate fiber of wFiber
   const oldHook =
     wFiber.alternate &&
     wFiber.alternate.hooks &&
     wFiber.alternate.hooks[state.getHookIndex()]
+  // Init a new hook
   const hook = {
-    state: oldHook ? oldHook.state : initial,
+    state: oldHook ? oldHook.state : initial, // use old state (if any), else use initial arg
     queue: [],
   }
 
-  const actions = oldHook ? oldHook.queue : []
+
+  // If there are previous actions queued up in oldHook, we need to traverse them
+  // in order to get the new correct value when the fiber updates.
+  const actions = oldHook ? oldHook.queue : [] 
   actions.forEach(action => {
     hook.state = action(hook.state)
   })
 
+  // schecdule a re-render with the action added to the new hook queue. 
+  // "Quit current work and restart from the current node with new updated states!!!" (;
   const setState = action => {
-    let cRoot = state.getCurrentRoot() 
+    let cRoot = state.getCurrentRoot() // current root for working subtree
     hook.queue.push(action)
+    // Create a new wip root to trigger a re-render
     state.setWipRoot(
       {
         dom: cRoot.dom,
@@ -26,11 +34,14 @@ export default function useState(initial) {
         alternate: cRoot,
       }
     )
+    // start work from the new root
     state.setNextUnitOfWork(state.getWipRoot())
+    // restart
     state.clearDeletions()
   }
 
+  // update hooks
   state.pushWipFiberHooks(hook)
-  state.setHookIndex(state.getHookIndex() + 1)
+  state.setHookIndex(state.getHookIndex() + 1) // Inc index to grab potential next hook state.
   return [hook.state, setState]
 }
